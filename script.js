96e2d5d3716642daae64766fa2316feefdfd7be7
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-31 22:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	let str=s.spilt(" ");
+	if(s.length==0){
+		return str=" "
+	}
+	for (let index = 0; index <str; index++) {
+		return str[0];
+	}
 }
 
 // Do not change the code below
